ee8259c22b88c74a3b51cabee08136ed27c4ec8a
Gabriel Torres <al2012.163.torres@gmail.com>
2021-10-17 17:02:15 -0300

    Implemented /categories get

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/categories", async (req, res) => {
+    try {
+        const result = await connection.query(`SELECT * FROM categories;`);
+        res.send(result.rows);
+    }
+    catch {
+        res.sendStatus(500);
+    }
+})
+
 app.listen(4000, () => {
   console.log('Server is listening on port 4000.');
 });
